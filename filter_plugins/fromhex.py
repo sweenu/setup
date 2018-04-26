8e0364c6312f3dca6926ac3361c05bd02fe04160
Bruno Inec <brunoinec@gmail.com>
2018-04-26 11:46:08 +0200

    Even more fixes, yay

--- filter_plugins/fromhex.py
+++ filter_plugins/fromhex.py
@@ -1,10 +1,11 @@
 def fromhex(hex_value):
     return tuple(ord(c) for c in str(hex_value).decode('hex'))
 
+
 class FilterModule():
     def filters(self):
         return {
-            'fromhex': fromhex
-            # When I'll use python3:
-            # 'fromhex': bytes.fromhex
+            # For Python2:
+            # 'fromhex': fromhex
+            'fromhex': bytes.fromhex
         }
